a9279ec7822e6a9c991bd9243b2eb3c9cef917ed
MacBook <macbook@MacBook-Pro-MacBook-2.local>
2020-01-29 20:33:57 +0200

    change  project

---
+++ js/main.js
@@ -0,0 +1,62 @@
+$('.js-slider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+
+let $page = $('html, body');
+$('a[href*="#"]').click(function () {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
+
+// navigation
+
+$(document).ready(function () {
+    $(".nav-block__link").click(function () {
+        $('.nav-block__link').removeClass('active-link');
+        $(this).addClass('active-link');
+    });
+});
+
+
+
+
+// SLIDER-navi
+
+$(document).ready(function () {
+    $(".navi-slider__sircle").click(function () {
+        $('.navi-slider .active-link-scroll').removeClass('active-link-scroll');
+        $(this).addClass('active-link-scroll');
+    });
+});
+
+// nav
+
+$(document).ready(function () {
+    $(".nav-block__link").click(function () {
+        $('.nav-block__link').removeClass('active-link-navi');
+        $(this).addClass('active-link-navi');
+    });
+});
